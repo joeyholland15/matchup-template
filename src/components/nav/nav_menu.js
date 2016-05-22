81d1ab1a68ea63c3c7abe73c2899dea4a87805de
Joey Holland <joeyholland15@Joeys-MacBook-Pro.local>
2016-05-22 09:31:53 -0700

    Adds conferences menu to sidebar and cleans up nav menu

--- src/components/nav/nav_menu.js
+++ src/components/nav/nav_menu.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router'
 const Navmenu = () => {
   return (
     <ul className="navmenu menu">
+      <li className="options image"><a href="#"><img src="../images/Drawing.png"/></a></li>
       <li className="options text"><Link to='/players'>Players</Link></li>
       <li className="options text"><Link to='/teams'>Teams</Link></li>
       <li className="options text"><Link to='/matchups'>Matchups</Link></li>
--- src/components/side/conference_menu.js
+++ src/components/side/conference_menu.js
@@ -7,8 +7,18 @@ export default class ConferenceMenu extends Component {
     return (
       <aside className="team-list col">
         <div className="side-col">
+          <Link to='/conferences/sec'><div className="side-row" id="sec"></div></Link>
+          <div className="side-row" id="pac"></div>
+          <div className="side-row" id="big12"></div>
+          <div className="side-row" id="b1g"></div>
+          <div className="side-row" id="acc"></div>
         </div>{/*}
         */}<div className="side-col">
+          <div className="side-row" id="aac"></div>
+          <div className="side-row" id="cusa"></div>
+          <div className="side-row" id="mac"></div>
+          <div className="side-row" id="mwc"></div>
+          <div className="side-row" id="nd"></div>
         </div>
       </aside>
     )
